21e128ac6791238b91aae25961082c77a6f63cd7
mamor <mamor.dev@gmail.com>
2012-10-14 05:52:40 +0900

    Added classes/input.php

---
+++ classes/input.php
@@ -0,0 +1,16 @@
+<?php
+
+class Input extends Fuel\Core\Input
+{
+
+	/**
+	 * Return's whether this is an PJAX request or not
+	 *
+	 * @return  bool
+	 */
+	public static function is_pjax()
+	{
+		return static::server('HTTP_X_PJAX') !== null;
+	}
+
+}
