fd09177c12753a46d2edd2329a058dc1c2d23f3d
mamor <mamor.dev@gmail.com>
2012-11-05 14:48:57 +0900

    Renamed php-cs-fixer to php-cs-fixer.phar

--- php-cs-fixer.sh
+++ php-cs-fixer.sh
@@ -2,4 +2,5 @@
 
 # http://cs.sensiolabs.org/
 
-php php-cs-fixer fix ./ --fixers=linefeed,trailing_spaces,unused_use,php_closing_tag,short_tag,return,visibility,phpdoc_params,eof_ending,extra_empty_lines,include,PSR0,controls_spaces,elseif
+php php-cs-fixer.phar self-update
+php php-cs-fixer.phar fix ./ --fixers=linefeed,trailing_spaces,unused_use,php_closing_tag,short_tag,return,visibility,phpdoc_params,eof_ending,extra_empty_lines,include,PSR0,controls_spaces,elseif
