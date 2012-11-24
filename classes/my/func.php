79bbbe1aaee52dcd1beb4b6955a03e9abaf92430
mamor <mamor.dev@gmail.com>
2012-11-24 20:37:08 +0900

    Added function My_Func::mk_hash($index, $min_length = 0)

--- classes/my/func.php
+++ classes/my/func.php
@@ -13,4 +13,35 @@ class My_Func
 		return $messages;
 	}
 
+	public static function mk_hash($index, $min_length = 0) {
+
+		$ascii_ranges = array(
+			array(48,57),  //0 to 9
+			array(65,90),  //A to Z
+			array(97,122), //a to z
+		);
+
+		$asciis = array();
+		foreach ($ascii_ranges as $ascii_range)
+		{
+			$asciis = array_merge($asciis, range($ascii_range[0], $ascii_range[1]));
+		}
+
+		$shuffles = array_keys($asciis);
+		srand(0);
+		shuffle($shuffles);
+
+		$asciis_count = count($asciis);
+		$i = 0;
+		$hashs = array();
+		do
+		{
+			$hashs[$i] = chr($asciis[$shuffles[(int) (floor($index / pow($asciis_count, $i)) + $i) % $asciis_count]]);
+			$i = count($hashs);
+		}
+		while (($min_length > $i) || (pow($asciis_count, $i) <= $index));
+
+		return implode("", $hashs);
+	}
+
 }
