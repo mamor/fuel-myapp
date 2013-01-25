ece1bc298194029d926d0e7c685946c6912550a6
madroom project <mamor.dev@gmail.com>
2013-01-25 18:34:32 +0900

    Update config/test/config.php

--- config/test/config.php
+++ config/test/config.php
@@ -4,6 +4,9 @@ return array(
 	'base_url' => null,
 	'log_threshold' => Fuel::L_DEBUG,
 	'locale' => null,
+	'merge_development_configs' => array(
+		'base_url',
+	),
 	'merge_development_config_names' => array(
 		'db',
 	),
