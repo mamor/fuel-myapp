16f392259ffd0922aa50e5e404a1c89b2efee033
mamor <mamor.dev@gmail.com>
2013-01-27 23:51:10 +0900

    Fixed FunctionalTestCase class

--- classes/functionaltestcase.php
+++ classes/functionaltestcase.php
@@ -19,9 +19,8 @@
 require_once APPPATH . 'vendor/Goutte/goutte.phar';
 use Goutte\Client;
 
-abstract class FunctionalTestCase extends \Fuel\Core\TestCase
+abstract class FunctionalTestCase extends DbTestCase
 {
-	const BASE_URL = 'http://localhost/fuelphp/';
 
 	protected static $client;  // Clientオブジェクト
 	protected static $crawler; // Crawlerオブジェクト
@@ -29,20 +28,42 @@ abstract class FunctionalTestCase extends \Fuel\Core\TestCase
 
 	public static function setUpBeforeClass()
 	{
+		parent::setUpBeforeClass();
+
 		// .htaccessをテスト環境用に変更
 		$htaccess = DOCROOT . 'public/.htaccess';
-		if ( ! file_exists($htaccess . '_develop'))
+		$htaccess_test = APPPATH . 'tests/_files/.htaccess.test';
+		$htaccess_develop = APPPATH . 'tests/_files/.htaccess.develop';
+		if ( ! file_exists($htaccess_develop))
 		{
-			$ret = rename($htaccess, $htaccess . '_develop');
+			$ret = rename($htaccess, $htaccess_develop);
 			if ($ret === false)
 			{
 				exit('Error: can\'t backup ".htaccess" !');
 			}
 		}
-		$ret = copy($htaccess . '_test', $htaccess);
+		$ret = copy($htaccess_test, $htaccess);
 		if ($ret === false)
 		{
-			exit('Error: can\'t copy ".htaccess_test" !');
+			exit('Error: can\'t copy ".htaccess.test" !');
+		}
+
+		// bootstrap.phpをテスト環境用に変更
+		$bootstrap = APPPATH . 'bootstrap.php';
+		$bootstrap_test = APPPATH . 'tests/_files/bootstrap.php.test';
+		$bootstrap_develop = APPPATH . 'tests/_files/bootstrap.php.develop';
+		if ( ! file_exists($bootstrap_develop))
+		{
+			$ret = rename($bootstrap, $bootstrap_develop);
+			if ($ret === false)
+			{
+				exit('Error: can\'t backup "bootstrap.php" !');
+			}
+		}
+		$ret = copy($bootstrap_test, $bootstrap);
+		if ($ret === false)
+		{
+			exit('Error: can\'t copy "bootstrap.php.test" !');
 		}
 
 		// GoutteのClientオブジェクトを生成
@@ -57,12 +78,15 @@ abstract class FunctionalTestCase extends \Fuel\Core\TestCase
 
 		// .htaccessを開発環境用に戻す
 		$htaccess = DOCROOT . 'public/.htaccess';
-		copy($htaccess . '_develop', $htaccess);
+		$htaccess_develop = APPPATH . 'tests/_files/.htaccess.develop';
+		copy($htaccess_develop, $htaccess);
+
+		// bootstrap.phpを開発環境用に戻す
+		$bootstrap = APPPATH . 'bootstrap.php';
+		$bootstrap_develop = APPPATH . 'tests/_files/bootstrap.php.develop';
+		copy($bootstrap_develop, $bootstrap);
+
+		parent::tearDownAfterClass();
 	}
 
-	// 絶対URLを返す
-	public static function open($uri)
-	{
-		return static::BASE_URL . $uri;
-	}
 }
