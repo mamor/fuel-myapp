6d322ff73f740ad889901d8686f8cc792ae425b9
mamor <mamor.dev@gmail.com>
2013-01-28 14:01:52 +0900

    Renamed ".gitignore.default" to ".gitignore"

--- _dl.sh
+++ _dl.sh
@@ -2,7 +2,7 @@
 
 APPPATH="./"
 
-wget -N -P ${APPPATH}../../ https://raw.github.com/mp-php/fuel-myapp/master/.gitignore.default --no-check-certificate
+wget -N -P ${APPPATH}../../ https://raw.github.com/mp-php/fuel-myapp/master/.gitignore --no-check-certificate
 wget -N -P ${APPPATH}../../ https://raw.github.com/mp-php/fuel-myapp/master/build.xml --no-check-certificate
 
 wget -N -P ${APPPATH} https://raw.github.com/mp-php/fuel-myapp/master/bootstrap_makegood.php --no-check-certificate
