db0f317f2b90e33cebc3cccd676901d71d87fa13
mamor <mamor.dev@gmail.com>
2013-01-28 13:51:40 +0900

    updated _dl.sh

--- _dl.sh
+++ _dl.sh
@@ -17,6 +17,9 @@ wget -N -P ${APPPATH}config/ https://raw.github.com/mp-php/fuel-myapp/master/con
 wget -N -P ${APPPATH}config/development/ https://raw.github.com/mp-php/fuel-myapp/master/config/development/config.php --no-check-certificate
 wget -N -P ${APPPATH}config/test/ https://raw.github.com/mp-php/fuel-myapp/master/config/test/config.php --no-check-certificate
 
+wget -N -P ${APPPATH}tests/_files/ https://raw.github.com/mp-php/fuel-myapp/master/tests/_files/.htaccess.test --no-check-certificate
+wget -N -P ${APPPATH}tests/_files/ https://raw.github.com/mp-php/fuel-myapp/master/tests/_files/bootstrap.php.test --no-check-certificate
+
 wget -N -P ${APPPATH}tasks/ https://raw.github.com/mp-php/fuel-myapp/master/tasks/coverage.php --no-check-certificate
 wget -N -P ${APPPATH}tasks/ https://raw.github.com/mp-php/fuel-myapp/master/tasks/generate.php --no-check-certificate
 wget -N -P ${APPPATH}tasks/ https://raw.github.com/mp-php/fuel-myapp/master/tasks/phpdoc.php --no-check-certificate
