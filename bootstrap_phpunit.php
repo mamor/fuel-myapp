6c3ab2eafdc789f7ae6466a7029329681baaeea9
mamor <mamor.dev@gmail.com>
2013-05-12 01:38:40 +0900

    renamed bootstrap_makegood.php to bootstrap_phpunit.php and updated bootstrap_phpunit.php

--- bootstrap_phpunit.php
+++ bootstrap_phpunit.php
@@ -1,24 +1,15 @@
 <?php
 
-// Load the PUPUnit Autoloader
-include_once('PHPUnit/Autoload.php');
-
 /**
  * Set error reporting and display errors settings.  You will want to change these when in production.
  */
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
-// for MakeGood
-$_SERVER['doc_root'] = '../../';
-$_SERVER['app_path'] = 'fuel/app';
-$_SERVER['core_path'] = 'fuel/core';
-$_SERVER['package_path'] = 'fuel/packages';
-$_SERVER['FUEL_ENV'] = 'test';
-
-$app_path		= rtrim($_SERVER['app_path'], '/').'/';
-$package_path	= rtrim($_SERVER['package_path'], '/').'/';
-$core_path		= rtrim($_SERVER['core_path'], '/').'/';
+$app_path     = rtrim($_SERVER['app_path'], '/').'/';
+$package_path = rtrim($_SERVER['package_path'], '/').'/';
+$vendor_path  = rtrim($_SERVER['vendor_path'], '/').'/';
+$core_path    = rtrim($_SERVER['core_path'], '/').'/';
 
 /**
  * Website docroot
@@ -27,10 +18,12 @@ define('DOCROOT', realpath(__DIR__.DIRECTORY_SEPARATOR.$_SERVER['doc_root']).DIR
 
 ( ! is_dir($app_path) and is_dir(DOCROOT.$app_path)) and $app_path = DOCROOT.$app_path;
 ( ! is_dir($core_path) and is_dir(DOCROOT.$core_path)) and $core_path = DOCROOT.$core_path;
+( ! is_dir($vendor_path) and is_dir(DOCROOT.$vendor_path)) and $vendor_path = DOCROOT.$vendor_path;
 ( ! is_dir($package_path) and is_dir(DOCROOT.$package_path)) and $package_path = DOCROOT.$package_path;
 
 define('APPPATH', realpath($app_path).DIRECTORY_SEPARATOR);
 define('PKGPATH', realpath($package_path).DIRECTORY_SEPARATOR);
+define('VENDORPATH', realpath($vendor_path).DIRECTORY_SEPARATOR);
 define('COREPATH', realpath($core_path).DIRECTORY_SEPARATOR);
 
 unset($app_path, $core_path, $package_path, $_SERVER['app_path'], $_SERVER['core_path'], $_SERVER['package_path']);
